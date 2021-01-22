1bae711f2b9c72cab3f30a283966419f2966b28e
xianingning-a <2210976302@qq.com>
2021-01-22 16:57:57 +0800

    项目创建成功，首页，详情，注册不是很完善

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,24 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+//登录路由配置
+router.get('/login',function(req,res){
+  res.render('login',{})
+})
+//文章详情页路由配置
+router.get('/details',function(req,res){
+  res.render('details',{})
+})
+//首页路由
+router.get('/home',function(req,res){
+  res.render('home',{})
+})
+//注册路由配置
+router.get('/registered',function(req,res){
+  res.render('registered',{})
+})
+//新曾页面路由配置
+router.get('/add',function(req,res){
+  res.render('add',{})
+})
 module.exports = router;
