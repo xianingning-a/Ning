d9269e6a53085918ee9714aed6eb8be087c2ef9d
xianingning-a <2210976302@qq.com>
2021-01-28 11:33:27 +0800

    编辑，删除功能完成

--- routes/article.js
+++ routes/article.js
@@ -12,36 +12,62 @@ var Multiparty = require('Multiparty')
 let fs = require('fs')
 
 //添加博客接口
-router.post('/cdd',(req,res,next)=>{
-    console.log(req.body);
-    let date = new Date();
-    console.log(date);
+router.post('/cdd', (req, res, next) => {
+  console.log(req.body);
+  // let date = new Date();
+  // console.log(date);
 
-    let articleInfo = {
-        titletxt:req.body.titletxt,
-        content:req.body.content,
-        datetime:date,
-      }
-    
+  //首先获取文章id，依次判断是新增还是编辑
+  let nId = req.body.dId || ''
+  // console.log(nId);
+  //新增
+
+  if (!nId) {
+    var articleInfo = {
+      titletxt: req.body.titletxt,
+      content: req.body.content,
+      datetime: Date.now(),
+    }
+
     //页面保单数据，放入模型
     let articleI = new Article(articleInfo)
-    
+
     //保存
-    articleI.save((err,result)=>{
+    articleI.save((err, result) => {
       if (!err) {
-        res.send(result)
+        // res.send(result)
+        res.redirect('/home')
       }
     })
+
+  } else {//编辑
+    let page = req.body.page
+
+    let articleInfo = {
+      titletxt: req.body.titletxt,
+      content: req.body.content,
+    }
+
+    Article.findByIdAndUpdate(nId, articleInfo, { new: true }, (err, result) => {
+      if (!err) {
+        res.redirect(`/home?page=${page}`)
+      }
+    })
+
+  }
+
+
+
 })
 
 //新增上传图片的路由
-router.post('/upload',(req,res,next)=>{
+router.post('/upload', (req, res, next) => {
   //图片文件上传的操作
   console.log(req.body);
   //实例化Multiparty的from
   let form = new Multiparty.Form()
   //使用path，获取文件信息
-  form.parse(req,(err,fields,files)=>{
+  form.parse(req, (err, fields, files) => {
     if (err) {
       console.log(err);
     }
@@ -58,12 +84,27 @@ router.post('/upload',(req,res,next)=>{
     //触发读写管道，实现上传
     rStream.pipe(wStream)
     //将文件返回ckeditor这个插件
-    wStream.on('close',()=>{
+    wStream.on('close', () => {
       res.send({
-        uploaded:1,
-        url:filePath
+        uploaded: 1,
+        url: filePath
       })
     })
   })
- })
+})
+
+
+//文章删除的接口
+router.get('/delete',(req,res,next)=>{
+  let id = req.query._id
+  let page = req.query.page
+  console.log(id,page);
+  //从后台删除一条数据
+  Article.deleteOne({_id:id},err => {
+    if (!err) {
+      // res.send('删除成功')
+      res.redirect(`/home?page=${page}`)
+    }
+  })
+})
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -7,26 +7,90 @@ var router = express.Router();
 //   res.render('index', { userName });
 // });
 
+//将用户模板导入
+let Article = require('../module/article')
+
+//导入时间插件
+let moment = require('moment')
+
 //登录路由配置
 router.get('/login',function(req,res){
   res.render('login',{})
 })
 //文章详情页路由配置
-router.get('/details',function(req,res){
-  res.render('details',{})
+router.get('/details', async function(req,res){
+  
+
+//在详情页渲染路由的时候接收一下传递过来的参数_id
+let blokId = req.query._id
+console.log(blokId);
+// 根据获取到的id使数据库查询方法，拿到当前文章详情
+ let data = await Article.findOne({_id:blokId})
+
+ data['time'] = moment(data.datetime).utcOffset(480).format('YYYY-MM-DD HH:mm:ss')
+
+let userName = req.session.username || ''
+  res.render('details',{userName,data})
 })
 //首页路由
-router.get('/home',function(req,res,next){
+router.get('/home', async function(req,res,next){
+  let cPage = req.query.page || 1
+  console.log(cPage);
+  // let data = await Article.find()
+  // console.log(data);
   let userName = req.session.username || ''
-  res.render('home',{userName})
+
+  let data = {
+    blogList:[],//文章列表
+    currPage:cPage,//当前页数
+    PagesTotle:'',//总页数
+  }
+  //设置每页渲染的条数
+  let pageSize = 2
+  //确定每页显示的数据
+  data.blogList = await Article.find()
+    .limit(pageSize)//限定展示出来的条数
+    .skip((data.currPage - 1) * pageSize)//限定从第几条开始截胡
+    //总数据
+    let blogAll = await Article.find()
+    //总页码
+    data.PagesTotle = blogAll.length/pageSize
+    //将所有的时间戳转换成时间
+    data.blogList.map(item => {
+      let a = moment(item.datetime).utcOffset(480).format('YYYY-MM-DD HH:mm:ss')
+      item['time']= a
+    })
+  
+
+  res.render('home',{userName,data})
 })
 //注册路由配置
 router.get('/registered',function(req,res){
   res.render('registered',{})
 })
 //新曾页面路由配置
-router.get('/add',function(req,res){
+router.get('/add', async function(req,res){
   let userName = req.session.username || ''
-  res.render('add',{userName})
+
+  let id = req.query._id || ''
+
+  if (id) {
+    let page = req.query.page
+    console.log(id);
+    console.log(page);
+    //文章数据查询渲染
+    let dataa = await Article.findOne({_id:id})
+    dataa.page = page
+    //时间处理
+    dataa['time'] = moment(dataa.datetime).utcOffset(480).format('YYYY-MM-DD HH:mm:ss')
+    res.render('add',{userName,dataa})
+  }else{
+    let dataa = {
+      titletxt:'',
+      content:'',
+    }
+    res.render('add',{userName,dataa})
+  }
+  
 })
 module.exports = router;
